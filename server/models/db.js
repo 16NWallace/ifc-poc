af7cbed825deb3e48c45931d96bd19afe6ac6e11
Nadia Wallace <wallace.nadia@gmail.com>
2016-01-11 15:58:50 -0500

    Endpoints added for getting data tables

--- server/models/db.js
+++ server/models/db.js
@@ -2,5 +2,5 @@ var pg = require('pg');
 var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query(""); //Insert SQL Query here
+var query = client.query(""); //Insert SQL Query here to set up the database
 query.on('end', function() { client.end(); });
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,15 +5,19 @@ var router = express.Router();
 var pg = require('pg');
 var connectionString = require(path.join(__dirname, '../', '../', 'config'));
 var path = require('path');
+var doingBizInd = ['gettingElectricity','startingBusiness', 'resolvingInsolvency', 'tradingAcrossBorders',
+'agg', 'gettingCredit', 'constructionPermit', 'enforcingContracts', 'payingTaxes', 'protectMinorities', 
+'registerProperty'];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname,  '../', '../', 'client', 'views', 'index.html'));
 });
 
-//Get data -- can pass query parameters (TODO: determine query parameters)
-router.get('/api/v1/doingbusiness/all', function(req, res){
-  var results = [];
+//Get all data
+//To-do: figure out query parameters as necessary
+router.get('/api/v1/doingbusiness/', function(req, res){
+  var results = {};
   //Get async pg client from pool
   pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
@@ -22,21 +26,20 @@ router.get('/api/v1/doingbusiness/all', function(req, res){
       console.log(err);
       return res.status(500).json({ success: false, data: err});
     }
-    //Use parameterized query to prevent SQL injection attack
-    var query = client.query("WRITE QUERY TO PULL ALL TABLES");
-    query.on('row', function(row){
-      results.push(row);
-    });
-    query.on('end', function(){
-      done();
-      return res.json(results);
-    });
+    //Add results to data structure
+    for(table_id in doingBizInd){
+      getTableById(client, table_id, function(res){
+        results[table_id]=res;
+      })
+    }
+    done();
+    res.json(results);
   });
   pg.end();
 });
 
 router.get('/api/v1/doingbusiness/:table_id', function(req, res){
-  var results = [];
+  var table_id = req.query.table_id;
   //Get async pg client from pool
   pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
@@ -45,19 +48,26 @@ router.get('/api/v1/doingbusiness/:table_id', function(req, res){
       console.log(err);
       return res.status(500).json({ success: false, data: err});
     }
-    //Use parameterized query to prevent SQL injection attack
-    var query = client.query();
-    query.on('row', function(row){
-      results.push(row);
-    });
-    query.on('end', function("**** CREATE QUERY ***"){
+    getTableById(client, table_id, function(res){
       done();
-      return res.json(results);
+      res.json({table_id:results}); //singleton JSON
     });
   });
   pg.end();
 });
 
+var getTableById = function(client, table_id, callback){
+  client.query("SELECT * FROM $1", [table_id], function(err, res){
+    //SQL Error
+    if(err){
+      done();
+      console.log(err);
+    }
+    query.on('end', function(){
+        callback(res);
+    })
+  })
+}
 //Future: one GET endpoint per dataset (DBI, WGI, etc)
 
 module.exports = router;
